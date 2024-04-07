b18fc71da0545d3d995cbe5f5c7f9f0a7f06cd52
Rebecca Gartenberg <rebeccagartenberg1@gmail.com>
2024-04-07 11:44:32 -0400

    Create /chat/completions POST request with content input

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # llm
 Online LLM
+
+##### Run API:
+cargo run
+
+##### Example of using the /chat/completions endpoint:
+
+curl http://localhost:8000/chat/completions -X POST -d '{"input": "what color is the sky? Limit to 10 words.}'
--- rust_rest_api/src/main.rs
+++ rust_rest_api/src/main.rs
@@ -4,13 +4,30 @@ use rocket::{get, post, routes, Rocket, Request, Data, Response};
 use rocket::http::Status;
 use rocket::http::ContentType;
 use rocket::response::Responder;
+use openai_api_rs::v1::api::Client;
+use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use openai_api_rs::v1::common::GPT4_TURBO_PREVIEW as GPT4;
+use std::env;
 
-#[get("/hello")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[post("/chat/completions", data = "<input>")]
+fn index(input: String) -> Result<(), Box<dyn std::error::Error>>{
+    let client = Client::new(env::var("OPENAI_API_KEY").unwrap().to_string());
+    let req = ChatCompletionRequest::new(
+        GPT4.to_string(),
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: chat_completion::Content::Text(String::from(input)),
+            name: None,
+        }],
+    );
+
+    let result = client.chat_completion(req)?;
+    println!("Content: {:?}", result.choices[0].message.content);
+    println!("Response Headers: {:?}", result.headers);
+
+    Ok(())
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
-
 }
